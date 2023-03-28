da24c92e05070e846a3db0fa382d4c34e300837b
mono0218 <81796635+mono0218@users.noreply.github.com>
2023-03-28 13:14:14 +0000

    fix: yomiage.js

--- function/yomiage.js
+++ function/yomiage.js
@@ -9,6 +9,7 @@ export default async function yomiage(msg,player){
     const rpc = axios.create({ baseURL: process.env.URL, proxy: false });
 
     const audio_query = await rpc.post('audio_query?text=' + encodeURI(msg.content) + '&speaker=1');
+    console.log("発音データを取得しました")
     const synthesis = await rpc.post("synthesis?speaker=1", JSON.stringify(audio_query.data), {
         responseType: 'stream',
         headers: {
@@ -16,6 +17,7 @@ export default async function yomiage(msg,player){
             "Content-Type": "application/json"
         }
     });
+    console.log("合成しました")
     let resource = createAudioResource(synthesis.data, { inputType: StreamType.Arbitrary, inlineVolume: true });
     player.on('error', error => {
         console.error('Error:', error.message, );
